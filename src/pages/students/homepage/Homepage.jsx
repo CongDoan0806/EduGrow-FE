f468d21c72f75b48791f31094494e8c6832b6292
ToNga156 <ngaichi2004@gmail.com>
2025-05-07 00:37:29 +0700

    Add-teacherlist-with-api-fetch/nga

---
+++ src/pages/students/homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TeacherList from '../../../components/students/homepage/TeacherList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Homepage = () => {
+  return (
+    <div>
+      <section className="my-5">
+        <TeacherList />
+      </section>
+    </div>
+  );
+};
+
+export default Homepage;
