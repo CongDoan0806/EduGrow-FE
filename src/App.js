545c054d857a56905f8e7246c93d3100d4f0d464
viWhnga <vihang05092005@gmail.com>
2025-05-09 21:53:48 +0700

    Add-student-sidebar/hang

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Homepage from './pages/students/homepage/Homepage';
+import StudentSidebar from './layouts/sidebars/StudentSidebar';
+import TeacherSidebar from './layouts/sidebars/TeacherSidebar';
 function App() {
   return (
     <div >
-    <Homepage/>
+    {/* <Homepage/> */}
+    <StudentSidebar/>
+    {/* <TeacherSidebar/> */}
 
     </div>
   );
