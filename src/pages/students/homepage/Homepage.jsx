63fbf6227d8af2548f1bc0e7fd5b2a8b9b18d893
ToNga156 <ngaichi2004@gmail.com>
2025-05-09 09:32:47 +0700

    Fix-css-and-Add-routes

--- src/pages/students/homepage/Homepage.jsx
+++ src/pages/students/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TeacherList from '../../../components/students/homepage/TeacherList';
+import './Homepage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Homepage = () => {
