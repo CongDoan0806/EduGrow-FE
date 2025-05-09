6a50590b407d0541edb988e1baf56dbacf82a624
ToNga156 <ngaichi2004@gmail.com>
2025-05-09 10:07:47 +0700

    Update-today-goals/nga

--- src/components/students/homepage/TodayGoals.jsx
+++ src/components/students/homepage/TodayGoals.jsx
@@ -6,13 +6,13 @@ const TodaysGoal = () => {
       <h1 className="todays-goal-title">Today's Goal</h1>
       
       <div className="todays-goal-subcontainer">
-        <div classname="todays-goal-background">
-            <p>Well, there are no classes today. <br /> Take a break and don’t forget to review your learning progress!</p>
-            <button>Check Calendar</button>
+        <div className="todays-goal-background">
+            {/* <p>Well, there are no classes today. <br /> Take a break and don’t forget to review your learning progress!</p>
+            <button>Check Calendar</button> */}
         </div>
 
         <div className="img-todays-goal">
-            <img src="./assets/images/pandaAtTodayGoals.png" alt="img-panda"></img>
+            {/* <img src="./assets/images/pandaAtTodayGoals.png" alt="img-panda"></img> */}
         </div>
 
         <div></div>
