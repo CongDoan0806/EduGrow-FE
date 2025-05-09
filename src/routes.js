3a52958daca1ce219317e3fa3f8969b94d5017df
ToNga156 <ngaichi2004@gmail.com>
2025-05-10 00:25:49 +0700

    Resolve-conflic/nga

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Homepage from "./pages/students/homepage/Homepage";
 import Login from "./pages/login/Login";
+import LearningJounal from "./pages/students/learning_journal/LearningJournal.jsx";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path : '/login',
 		exact : true,
 		main : ()=> <Login />
+  },
+  {
+    path: '/learningJournal',
+    exact: true,
+    main: () => <LearningJounal />
   }
 ];
 
